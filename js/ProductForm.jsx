768e4a52c4c4f720ac7b4329a1aebbde58704278
Sho Nguyen <sho417@sbcglobal.net>
2017-10-11 20:54:05 -0700

    state changes when input changes

--- js/ProductForm.jsx
+++ js/ProductForm.jsx
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const ProductForm = props => {
-  console.log(props);
-  return (
-    <div>
-      <input type="text" placeholder="Enter Name" />
-      <select name="categories" id="categories">
-        {props.categories.map(category => <option value={category}>{category}</option>)}
-      </select>
-      <input type="text" placeholder="Enter Year" />
-      <select name="seasons" id="seasons">
-        {props.seasons.map(season => <option value={season}>{season}</option>)}
-      </select>
-      <input type="text" placeholder="Enter Brand" />
-      <input type="text" placeholder="Enter Description" />
-    </div>
-  );
-};
+class ProductForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      categories: 'Jackets',
+      year: '',
+      seasons: 'SS',
+      brand: '',
+      description: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    this.setState({
+      [target.name]: target.value
+    });
+  }
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Enter Name" name="name" onChange={this.handleChange} />
+        <select name="categories" id="categories" onChange={this.handleChange}>
+          {this.props.categories.map(category => <option value={category}>{category}</option>)}
+        </select>
+        <input type="number" placeholder="Enter Year" name="year" onChange={this.handleChange} />
+        <select name="seasons" id="seasons" onChange={this.handleChange}>
+          {this.props.seasons.map(season => <option value={season}>{season}</option>)}
+        </select>
+        <input type="text" placeholder="Enter Brand" name="brand" onChange={this.handleChange} />
+        <input type="text" placeholder="Enter Description" name="description" onChange={this.handleChange} />
+        <button type="submit"> Add Product</button>
+      </div>
+    );
+  }
+}
 
 ProductForm.propTypes = {
-  categories: PropTypes.arrayOf(PropTypes.object),
-  seasons: PropTypes.arrayOf(PropTypes.object)
+  categories: PropTypes.arrayOf(PropTypes.string),
+  seasons: PropTypes.arrayOf(PropTypes.string)
 };
 ProductForm.defaultProps = { categories: [], seasons: [] };
 
